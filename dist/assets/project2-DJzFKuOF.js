eaff8e093bc7ad3ae2778814755d2569a556664f
tom <tom@tom-iMac.local>
2025-08-01 23:42:24 +0200

    init

---
+++ dist/assets/project2-DJzFKuOF.js
@@ -0,0 +1,13 @@
+const t=`---
+title: Projekt 2
+date: '2025-07-15'
+tags: ['react', 'typescript']
+description: 'Popis projektu'
+lang: 'cs'
+translationKey: 'project-one'  # Stejný klíč jako v EN verzi
+slug: 'projekt-2'              # Lokalizovaný slug
+image: https://dummyimage.com/600x400/000/fff
+---
+
+Obsah v češtině...
+`;export{t as default};
